e5ec4c261c404b24801685dfde643e662bbdaac2
ArielOliveira <ariel.oliveira01@gmail.com>
2017-11-02 23:39:08 -0300

    questao_1 adicionada

---
+++ src/questao_1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <vector>
+using std::vector;
+
+template<typename InputIterator>
+InputIterator closest2mean(InputIterator first, InputIterator last) {
+	int avg = 0, count = 0;
+	for (InputIterator it = first; it != last; it++) {
+		avg += *it;
+		count++;
+	}
+	avg /= 6;
+	InputIterator result = first;
+	bool smaller = true;
+	while (smaller) {
+		if (*result < avg){
+			smaller = true;
+			result++;	
+		} 
+		else smaller = false;
+	}
+
+	return result;
+
+}
+
+int main() {
+	vector<int> v { 1, 2, 3, 30, 40, 50 };
+	auto result = closest2mean(v.begin(), v.end());
+	cout << (*result) << endl;
+
+	return 0;
+}
